8bc5df2a0590fcf8446cca52d51bed87777c4883
WillXusd <33019963+WillXusd@users.noreply.github.com>
2018-11-22 16:53:07 +0800

    pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { BindComponent } from './bind/bind.component';
 import { FormsModule } from '@angular/forms';
 import { ConduitComponent } from './conduit/conduit.component';
+import { MultiplePipe } from './pipe/multiple.pipe';
 @NgModule({
   declarations: [
     AppComponent,
     BindComponent,
-    ConduitComponent
+    ConduitComponent,
+    MultiplePipe
   ],
   imports: [
     BrowserModule,
--- src/app/conduit/conduit.component.ts
+++ src/app/conduit/conduit.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ConduitComponent implements OnInit {
 
   birthday: Date = new Date();
+  pi: number = 3.1415926;
+  size: number = 7;
   constructor() { }
 
   ngOnInit() {
